7c6c38789694a4222bae206ada330d07785fafba
sahil97 <sahil.sharma071997@gmail.com>
2020-05-03 18:21:41 +0530

    added media queries for IPAD

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel(
-        {
-            margin: 5
-        }
-    );
+    $(".owl-carousel").owlCarousel({
+        slideSpeed : 200,
+        autoPlay : true,
+        margin: 5,
+        pagination : false,
+        responsiveClass:true,
+     responsive:{
+         0:{
+             items:2,
+         },
+         320: {
+            items:3,
+         },
+         600:{
+             items:4,
+             nav:false
+         },
+         1000:{
+             items:5,
+             loop:false
+         }
+     }
+        });
 
     $('.accordion').accordion({
         "transitionSpeed": 400
